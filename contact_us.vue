33bc48ab66d9db5178ec188d3e4e04922f738a99
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-21 10:39:19 -0500

    Centerpoint Mall 1.0  /contact_us.vue

--- contact_us.vue
+++ contact_us.vue
@@ -61,7 +61,7 @@
             <div style="padding-top: 40px;"></div>    
         </div>
         <div style="margin-bottom:-30px">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1234.5887233230621!2d-79.3461581825161!3d43.70531345482354!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4ccfb4f9d01f3%3A0xf925df5c38dba5e5!2sEast+York+Town+Centre!5e0!3m2!1sen!2sca!4v1545169043397" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2879.8909756114263!2d-79.42448398449761!3d43.7958752791166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2cfe2fd16f43%3A0xc5d9645af2ab219!2sCenterpoint+Mall!5e0!3m2!1sen!2sca!4v1545406728624" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </div>
 </template>
