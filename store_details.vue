2b6aed804ee56ecb9e78b0d67229c846006ddbcb
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-03 15:45:52 -0500

    Centerpoint Mall 1.0  /store_details.vue

--- store_details.vue
+++ store_details.vue
@@ -93,7 +93,7 @@
 	}
 </style>
 <script>
-    define(['Vue', 'vuex', 'moment', "jquery", "smooth-zoom", "vue!png-map", 'vue-lazy-load'], function(Vue, Vuex, moment, $, smoothZoom, PNGMapComponent, VueLazyload) {
+    define(['Vue', 'vuex', 'moment', "vue!mapplic-map", 'vue-lazy-load'], function(Vue, Vuex, moment, MapplicComponent, VueLazyload) {
         Vue.use(VueLazyload);
         return Vue.component("store-details-component", {
             template: template, // the variable template will be injected,
