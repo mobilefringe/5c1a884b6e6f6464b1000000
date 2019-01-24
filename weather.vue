b696cc6b21ebacf18e755d2962e1640430bbd633
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-24 16:10:34 -0500

    Centerpoint Mall 1.0  /weather.vue

--- weather.vue
+++ weather.vue
@@ -12,6 +12,31 @@
         Vue.use(Lightbox);
         return Vue.component("weather-component", {
             template: template, // the variable template will be injected,
+            computed: {
+                
+                weather() {
+                    var openWeatherAPIKey = "c225dc6aeb2d6a1b39e8a463cedb62bd";
+                    var openWeatherURL = "https://api.openweathermap.org/data/2.5/weather"+ "?id=6167865&APPID=" + openWeatherAPIKey + "&units=metric";
+                    
+                    var all_weather = {};
+                    var vm = this;
+                    axios.post(openWeatherURL).then(response => {
+                     (response);
+                      if(response && response.data && response.data.weather){
+                        
+                        var weather = response.data.weather[0];
+                        weather.image_url = "http://openweathermap.org/img/w/"+weather.icon+".png"
+                        vm.$set(all_weather, 'dets', weather)
+                        vm.$set(all_weather, 'main', response.data.main);
+                        vm.weatherDets = all_weather;
+                      }
+                    }).catch(error => {
+                      console.log("Data load error: " + error.message);
+                      (error);
+                       return all_weather;
+                    });
+                }
+            }
         });
     });
 </script>
