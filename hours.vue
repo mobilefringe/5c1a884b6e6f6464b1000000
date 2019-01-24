9be9feaf9bd3ac011c31ea916d1757cc93a58905
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-24 11:55:47 -0500

    Centerpoint Mall 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -11,7 +11,7 @@
         <div class="site_container">
             <div class="row hours_row">
                 <div class="col-sm-12 col-lg-3">
-                    <div class="hours_container todays_hours">
+                    <div class="hours_container ">
                         <p v-if="todaysHours && !todaysHours.is_closed" class="caps center">Open Now</p>
                         <p v-if="todaysHours && !todaysHours.is_closed" class="center">
                             {{todaysHours.open_time | moment("h:mm a", timezone)}} - {{todaysHours.close_time | moment("h:mm a", timezone)}}    
