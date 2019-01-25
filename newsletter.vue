4bfe5ba5f5e3b08abb4151d3b04695e8e7d25f1a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-25 14:46:56 -0500

    Centerpoint Mall 1.0  /newsletter.vue

--- newsletter.vue
+++ newsletter.vue
@@ -17,9 +17,7 @@
                                 <label class="label" for="cm-name">{{$t("newsletter_page.name")}}</label>
                                 <input v-model="form_data.name" required class="form-control" name="cm-name" type="text" placeholder="Name" id="cm-name">
                             </div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-sm-6 col-xs-12">
+                             <div class="col-sm-6 col-xs-12">
                                 <label class="label" for="newsletter_email">{{$t("newsletter_page.email")}}</label>
                                 <input v-model="form_data.email" required class="form-control js-cm-email-input" name="cm-gmkjr-gmkjr" type="email" placeholder="Email" id="newsletter_email">
                             </div>
