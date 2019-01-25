1ed123f728b01acbc6b2259ad1dc66bc4848297e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-01-25 16:31:30 -0500

    Centerpoint Mall 1.0  /main.js

--- main.js
+++ main.js
@@ -162,7 +162,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
                     // this.property.name = "Bramalea City Centre";
                     this.property.mm_host = this.property.mm_host.replace("http:", "");
                     // avoid making LOAD_META_DATA call for now as it will cause the entire Promise.all to fail since no meta data is set up.
-                    let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores"),this.$store.dispatch("getData", "repos")]);
+                    let results = await Promise.all([this.$store.dispatch("INITIALIZE_LOCALE"), this.$store.dispatch("getData", "hours"), this.$store.dispatch("getData", "stores"), this.$store.dispatch("getData", "promotions"), this.$store.dispatch("getData", "events"),this.$store.dispatch("getData", "jobs"),this.$store.dispatch("getData", "repos")]);
                     await Promise.all([this.$store.dispatch("LOAD_META_DATA")]);
                     return results;
                 } catch (e) {
